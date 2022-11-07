746d799dd06dbca1e126abfce0e7ffab788291b4
sanjaybaskar <sanjay.baskar1828@gmail.com>
2022-11-07 17:18:38 +0530

    VPC architecture file added

--- VPC.tf
+++ VPC.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 resource "aws_vpc" "myvpc" {
@@ -7,48 +7,47 @@ resource "aws_vpc" "myvpc" {
 }
 
 resource "aws_internet_gateway" "myigw" {
-  vpc_id = aws_vpc.myvpc.id 
-
+  vpc_id = aws_vpc.myvpc.id
 }
 
-  resource "aws_subnet" "mypubsub" {
+resource "aws_subnet" "mypubsub" {
   vpc_id     = aws_vpc.myvpc.id
   cidr_block = "192.168.1.0/24"
-  availability_zone = "us-east-1a"
-  }
+  availability_zone = "ap-south-1a"
+}
 
-  resource "aws_subnet" "myprivsub" {
+resource "aws_subnet" "myprivsub" {
   vpc_id     = aws_vpc.myvpc.id
-  cidr_block = "192.168.1.0/24"
-  availability_zone = "us-east-1b"
-  }
+  cidr_block = "192.168.2.0/24"
+  availability_zone = "ap-south-1b"
+}
+resource "aws_route_table" "mypubroutetb" {
+  vpc_id = aws_vpc.myvpc.id
+}
 
-  resource "aws_route_table" "mypubroutetb" {
-    vpc_id = aws_vpc.myvpc.id
-  }
+resource "aws_route_table" "myprivroutetb" {
+  vpc_id = aws_vpc.myvpc.id
+}
 
-    resource "aws_route_table" "myprivroutetb" {
-    vpc_id = aws_vpc.myvpc.id
-  }
-
-  resource "aws_route" "pub_route" {
+resource "aws_route" "pub_route" {
   route_table_id            = aws_route_table.mypubroutetb.id
   destination_cidr_block    = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.myigw.id
+  gateway_id   = aws_internet_gateway.myigw.id
 }
 
-  resource "aws_eip" "myeip" {
-  instance = aws_instance.web.id
-  vpc      = true
+resource "aws_eip" "myeip" {
 }
 
-  resource "aws_nat_gateway" "mynatgw" {
+resource "aws_nat_gateway" "mynat" {
   allocation_id = aws_eip.myeip.id
   subnet_id     = aws_subnet.mypubsub.id
-  }
+}
 
-     resource "aws_route" "priv_route" {
+resource "aws_route" "priv_route" {
   route_table_id            = aws_route_table.myprivroutetb.id
   destination_cidr_block    = "0.0.0.0/0"
-  gateway_id = aws_nat_gateway.mynatgw.id
+  gateway_id   = aws_nat_gateway.mynat.id
 }
+
+
+
