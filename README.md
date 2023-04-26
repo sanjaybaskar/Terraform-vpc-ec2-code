683314a9317e23aebb9b49938841dc1345f82a05
sanjaybaskar <sanjay.baskar1828@gmail.com>
2023-04-26 19:21:09 +0530

    Ec2 files updated

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 Terraform IaC code for VPC and EC2 instance deployment.
 
+This repository contains the VPC, EC2 instance creation Terraform IaC code.
+
+- Ec2-harcoded-values and VPC folder contains the code with hardcoded values.
+
+- EC2-module have the module template for EC2 instance creation without hardcoded values.
+
+Note: Edit buckt_name, vpc_id, subnet_id
--- ec2-module/ec2.tf
+++ ec2-module/ec2.tf
@@ -2,26 +2,30 @@
 
 data "aws_ami" "myami" {
   most_recent      = true
-  owners           = ["277477295271"]
+ // owners           = ["self"]
 
-  filter {
+ /* filter {
     name   = "name"
     values = ["Ec2-*"]
-  }
+  } */
 }
 
 resource "aws_instance" "myec2"{
     ami = data.aws_ami.myami.id
     key_name = var.key_name
     instance_type = var.instancetype
-    subnet_id = "subnet-0273cf69d7ce9a746"
+    subnet_id = "subnet-06de958156b1c8056"
     security_groups = [aws_security_group.mysg.id]
+
+    tags = {
+    Name = "sk-instance"
+  }
 }
 
 resource "aws_security_group" "mysg" {
   name        = var.sg_name
   description = "Allow port 22"
-  vpc_id      = "vpc-00edf82a4c8e9074b"
+  vpc_id      = "vpc-0110f21bc838c9df0"
 
 
   ingress {
