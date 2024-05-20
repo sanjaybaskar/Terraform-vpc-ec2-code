7209f32471ec39963e98ea27cb5e2e1a4639e8b8
sanjaybaskar <sanjay.baskar1828@gmail.com>
2024-05-20 23:08:31 +0530

    updated ec2 file

--- ec2-hardcoded-values/ec2.tf
+++ ec2-hardcoded-values/ec2.tf
@@ -1,7 +1,8 @@
-provider "aws"{
-    region ="ap-south-1"
+provider "aws" {
+  region = "us-east-1"
 }
 
+/*
 data "aws_ami" "myami" {
   most_recent      = true
   owners           = ["277477295271"]
@@ -12,44 +13,48 @@ data "aws_ami" "myami" {
   }
 }
 
-resource "aws_instance" "myec2"{
-    ami = data.aws_ami.myami.id
-    key_name = var.key_name
-    instance_type = var.instancetype
-    subnet_id = "subnet-0273cf69d7ce9a746"
-    security_groups = [aws_security_group.mysg.id]
-}
+*/
+
+
 
 resource "aws_security_group" "mysg" {
   name        = "sk-sg"
   description = "Allow port 22"
-  vpc_id      = "vpc-00edf82a4c8e9074b"
+  vpc_id      = "vpc-06a2756e0ec9dbe33"
 
 
   ingress {
-    description      = "TLS from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    description      = "TLS from VPC"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
     Name = "allow_tls"
   }
+}
+
+resource "aws_instance" "myec2" {
+  ami             = "ami-080e1f13689e07408"
+  key_name        = var.key_name
+  instance_type   = var.instancetype
+  subnet_id       = "subnet-0d020016c24231b6e"
+  security_groups = [aws_security_group.mysg.id]
 }
